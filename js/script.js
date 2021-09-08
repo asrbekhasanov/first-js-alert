f161d190fc984810f127900ad6d552b0d896a133
asrbekhasanov <asrbek.hasanov@gmail.com>
2021-09-08 15:34:40 +0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var welcome = "Keldizmiyee bratan";
+var offer = "Qani endi choy poydan olingchi";
+var fire = "Suring endi bottan :)";
+alert(welcome);
+alert(offer);
+var name_question = "Ismiz nimeydi o'zi?";
+var name = "Falonchijon";
+prompt(name_question, name);
+var work_question = "Oka nima ish qilas o'zi?";
+var work_name = "Programmistman";
+prompt(work_question, work_name);
+alert("A siz konditsionerimmi koriberolismi?")
+alert("Tre yoqole oldimdan :(");
+alert(fire);
